84c1399012f5a4a21e51c2895012d913e027b256
Tim Kelty <tim@fusionary.com>
2016-05-09 10:04:15 -0400

    style: uniform code style

--- test/unit/index.js
+++ test/unit/index.js
@@ -7,9 +7,9 @@ var utils = require('../../index');
 var path = require('path');
 var grunt = require('grunt');
 
-describe('utils', function () {
+describe('utils', function() {
   var shipit, task;
-  beforeEach(function () {
+  beforeEach(function() {
     shipit = new Shipit({
       environment: 'test',
       log: sinon.stub()
@@ -29,30 +29,30 @@ describe('utils', function () {
     };
   });
 
-  describe('#getShipit', function () {
-    it('should return Shipit', function (done) {
+  describe('#getShipit', function() {
+    it('should return Shipit', function(done) {
       expect(utils.getShipit(shipit)).to.equal(shipit);
       expect(utils.getShipit(grunt)).to.equal(shipit);
       done();
     });
   });
 
-  describe('#registerTask', function () {
-    it('should register a Grunt task', function (done) {
+  describe('#registerTask', function() {
+    it('should register a Grunt task', function(done) {
       utils.registerTask(grunt, 'test', task);
       expect(grunt.task.exists('test')).to.equal(true);
       done();
     });
 
-    it('should register a Shipit task', function (done) {
+    it('should register a Shipit task', function(done) {
       utils.registerTask(shipit, 'test', task);
       expect(shipit.hasTask('test')).to.equal(true);
       done();
     });
   });
 
-  describe('#equalValues', function () {
-    it('should return false', function (done) {
+  describe('#equalValues', function() {
+    it('should return false', function(done) {
       expect(utils.equalValues([
         {stdout: '/remote/deploy/releases/20141704123138'},
         {stdout: '/remote/deploy/releases/20141704123137'}
@@ -61,8 +61,8 @@ describe('utils', function () {
     });
   });
 
-  describe('#runTask', function () {
-    it('should return Grunt', function (done) {
+  describe('#runTask', function() {
+    it('should return Grunt', function(done) {
       expect(utils.runTask(grunt, 'test')).to.have.property('registerTask');
       done();
     });
